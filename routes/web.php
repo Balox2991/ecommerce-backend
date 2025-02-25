e764da2eefb0dedf8dd7e8c412ba1bbcfd816296
Juan David Amaya Quintero <jamaya215@unab.edu.co>
2025-02-25 15:23:00 -0500

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,29 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/products', function () {
+      echo"LISTADO DE PRODUCTOS DEL ECOMMERCE";
+});
+
+Route::get('/products/create', function () {
+    echo"FORMULARIO PARA CREAR UN PRODUCTO : ";
+});
+
+Route::get('/products/{name}/{categoria?}', function ($name,$categoria=null) {
+    if ($categoria == null){
+        echo"El producto es : $name";
+    }else{
+        echo " el producto es $name , y su categoria es $categoria ";
+    }
+    
+});
+
+
+
+
+
+
+
+
