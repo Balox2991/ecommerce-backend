6a98d78b17d9ab06a6f75abfeb2fe4b878f0a7bd
Zabala <czabala41@unab.edu.co>
2025-03-04 15:09:33 -0500

    create

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,68 @@
     <title>Backend</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
     <style>
+
+         /* Estilo para el encabezado */
+         .header-bg {
+            background-color: #FF6600;
+            color: white;
+            padding: 15px 0;
+        }
+
+        /* Estilo para los botones */
+        .btn-orange {
+            background-color: #FF6600;
+            border-color: #FF6600;
+            color: white;
+        }
+
+        .btn-orange:hover {
+            background-color: #e65c00;
+            border-color: #e65c00;
+        }
+
+        /* Estilo para el navbar */
+        .navbar {
+            background-color: #FF6600;
+        }
+
+        .navbar .navbar-brand,
+        .navbar .nav-link {
+            color: white;
+        }
+
+        .navbar .navbar-toggler-icon {
+            background-color: white;
+        }
+
+        .navbar .nav-link:hover {
+            color: #e65c00;
+        }
+
+        /* Margen entre los formularios */
+        .form-container {
+            margin-top: 30px;
+        }
+
+        /* Asegura que el footer se quede abajo en la página */
+        html, body {
+            height: 100%;
+        }
+
+        .content {
+            min-height: 100%;
+            margin-bottom: 50px; /* Espacio para el footer */
+        }
+
+        footer {
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            background-color: #FF6600; /* El mismo color que el botón */
+            color: white;
+            text-align: center;
+            padding: 15px 0;
+        }
         /* Fondo de la imagen de la UNAB */
         .unab-bg {
             background-image: url('https://unired.edu.co/images/instituciones/UNAB/2017/junio/unab.jpg');
